cb3af2a947de840be02cd869a77eb4544f6b7d15
Martin Ledgard <martin@le6o.com>
2015-08-23 08:39:37 +0100

    Guardsman class with support for null and empty checks

---
+++ src/Guardsman.php
@@ -0,0 +1,38 @@
+<?php namespace Guardsman;
+
+class Guardsman
+{
+    /** @var mixed */
+    protected $subject;
+
+    /** @param mixed $subject The subject being checked. */
+    public function __construct($subject)
+    {
+        $this->subject = $subject;
+    }
+
+    /**
+     * @throws \InvalidArgumentException if $subject is null.
+     */
+    public function isNotNull()
+    {
+        if ($this->subject === null) {
+            throw new \InvalidArgumentException('Subject should not be null');
+        }
+    }
+
+    /**
+     * @throws \UnexpectedValueException if $subject is an empty string when trimmed.
+     * @throws \UnexpectedValueException if $subject is empty.
+     */
+    public function isNotEmpty()
+    {
+        if (is_string($this->subject) && empty(trim($this->subject))) {
+            throw new \UnexpectedValueException('Subject must not be an empty string when trimmed');
+        }
+
+        if (empty($this->subject)) {
+            throw new \UnexpectedValueException('Subject must not be empty');
+        }
+    }
+}
